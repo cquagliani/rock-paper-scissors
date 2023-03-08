9a267c25cbd7e3310dd05df9b00a39bbfde2bd61
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-07 23:14:26 -0800

    Implement game logic for single round

--- index.js
+++ index.js
@@ -7,3 +7,40 @@ getComputerChoice = () => {
 
     return computerChoice;
 }
+
+playRound = (playerSelection, computerSelection) => {
+
+    let winner;
+    let message;
+    let playerSelectionMod = playerSelection.toUpperCase();
+    let computerSelectionMod = computerSelection.toUpperCase();
+
+    if (playerSelectionMod == computerSelectionMod) {
+        winner = 'Tie';
+    }
+    else if (playerSelectionMod == 'ROCK' && computerSelectionMod == 'SCISSORS') {
+        winner = playerSelectionMod;
+    } else if (playerSelectionMod== 'SCISSORS' && computerSelectionMod == 'PAPER') {
+        winner = playerSelectionMod;
+    } else if (playerSelectionMod== 'PAPER' && computerSelectionMod == 'ROCK') {
+        winner = playerSelectionMod;
+    } else {
+        winner = computerSelectionMod;
+    }
+
+    if (winner == 'Tie') {
+        message = 'You tied! Try again.';
+    } else if (winner == playerSelectionMod) {
+        message = `Congratulations! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        message = `You lost! ${computerSelection} beats ${playerSelection}.`;
+    }
+
+    return message;
+}
+
+
+// Call program to test
+const playerSelection = 'paper';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
