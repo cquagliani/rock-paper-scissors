ab33aeccb2cfa791767248b5732e055c544cb851
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-17 22:55:22 -0700

    Begin adding tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
+  purge: [
+    "./src/**/*.{html, js}",
+  ],
   corePlugins: {
     preflight: false,
   },
   content: [    
-    "./src/**/*.{html, js}", 
-    "./src/**/*"
+    "./src/**/*.{html, js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Sora', 'Inter', 'sans-serif'],
+        body: ['Sora', 'Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#6AFFAE',
+        secondary: '#5CE2FF',
+        tertiary: '#1F2021',
+      }
+    },
   },
   plugins: [],
 }
