8a2edd18fcd156189e3845ef2642341a75a287a8
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-18 17:07:23 -0700

    Fix game log color error

--- src/index.js
+++ src/index.js
@@ -4,153 +4,156 @@ let computerScore = 0;
 let combinedScore = 0;
 let finalGameMessage, overallWinner;
 
-/// Functions /// 
+/// Functions ///
 function getComputerChoice() {
-    const arr = ['Rock', 'Paper', 'Scissors'];
+	const arr = ["Rock", "Paper", "Scissors"];
 
-    let computerChoice = arr[Math.floor(Math.random()*arr.length)];
-    console.log(`Computer choice: ${computerChoice}`);
+	let computerChoice = arr[Math.floor(Math.random() * arr.length)];
+	console.log(`Computer choice: ${computerChoice}`);
 
-    return computerChoice;
+	return computerChoice;
 }
 
 function formatText(text) {
-    let formattedText = text.charAt(0).toUpperCase() + text.toLowerCase().slice(1);
-    return formattedText
+	let formattedText = text.charAt(0).toUpperCase() + text.toLowerCase().slice(1);
+	return formattedText;
 }
 
 function playRound(playerSelection) {
-    let computerSelection = getComputerChoice(); 
-    const playerSelectionMod = playerSelection.toUpperCase();
-    const computerSelectionMod = computerSelection.toUpperCase();
-    let winner, message;
-    const messagesList = {
-        tie: 'You tied! Try again.', 
-        player: `Congratulations! ${playerSelection} beats ${computerSelection}.`, 
-        computer: `You lost! ${computerSelection} beats ${playerSelection}.`
-    };
+	let computerSelection = getComputerChoice();
+	const playerSelectionMod = playerSelection.toUpperCase();
+	const computerSelectionMod = computerSelection.toUpperCase();
+	let winner, message;
 
-    if (playerSelectionMod == computerSelectionMod) {
-        winner = "Tie";
-        message = messagesList["tie"];
-    }
-    else if (playerSelectionMod == 'ROCK' && computerSelectionMod == 'SCISSORS') {
-        winner = "Player";
-        message = messagesList["player"];
-    } else if (playerSelectionMod== 'SCISSORS' && computerSelectionMod == 'PAPER') {
-        winner = "Player";
-        message = messagesList["player"];
-    } else if (playerSelectionMod== 'PAPER' && computerSelectionMod == 'ROCK') {
-        winner = "Player";
-        message = messagesList["player"];
-    } else {
-        winner = "Computer";
-        message = messagesList["computer"];
-    }
+	if (playerSelectionMod == computerSelectionMod) {
+		winner = "Tie";
 
-    // Update game log with player & computer picks
-    const formattedPlayer = formatText(playerSelectionMod);
-    const formattedComputer = formatText(computerSelectionMod);
+	} else if (
+		playerSelectionMod == "ROCK" &&
+		computerSelectionMod == "SCISSORS"
+	) {
+		winner = "Player";
 
-    const playerPickParagraph = document.createElement("p");
-    const playerPickNode = document.createTextNode(`${formattedPlayer}`);
-    playerPickParagraph.appendChild(playerPickNode);
+	} else if (
+		playerSelectionMod == "SCISSORS" &&
+		computerSelectionMod == "PAPER"
+	) {
+		winner = "Player";
 
-    const computerPickParagraph = document.createElement("p");
-    const computerPickNode = document.createTextNode(`${formattedComputer}`);
-    computerPickParagraph.appendChild(computerPickNode);
+	} else if (playerSelectionMod == "PAPER" && computerSelectionMod == "ROCK") {
+		winner = "Player";
 
-    // Change the color of the game log depending on who won the round
-    if (winner == "Tie") {
-        playerPickParagraph.classList.add('text-slate-50'); 
-        computerPickParagraph.classList.add('text-slate-50');
-    } else {
-        winner == "Player" ? playerPickParagraph.classList.add('text-blue-600') : playerPickParagraph.classList.add('text-red-600');
-        winner == "Computer" ? computerPickParagraph.classList.add('text-blue-600') : computerPickParagraph.classList.add('text-red-600');
-    }
+	} else {
+		winner = "Computer";
 
+	}
 
-    const playerElement = document.querySelector('#playerPick');
-    playerElement.appendChild(playerPickParagraph);
+	// Update game log with player & computer picks
+	const formattedPlayer = formatText(playerSelectionMod);
+	const formattedComputer = formatText(computerSelectionMod);
 
-    const computerElement = document.querySelector('#computerPick');
-    computerElement.appendChild(computerPickParagraph);
-    
-    
-    return winner;
+	const playerPickParagraph = document.createElement("p");
+	const playerPickNode = document.createTextNode(`${formattedPlayer}`);
+	playerPickParagraph.appendChild(playerPickNode);
+
+	const computerPickParagraph = document.createElement("p");
+	const computerPickNode = document.createTextNode(`${formattedComputer}`);
+	computerPickParagraph.appendChild(computerPickNode);
+
+	// Change the color of the game log depending on who won the round
+	if (winner == "Tie") {
+		playerPickParagraph.classList.add("text-gray");
+		playerPickParagraph.classList.add("font-light");
+		computerPickParagraph.classList.add("text-gray");
+		computerPickParagraph.classList.add("font-light");
+	} else {
+		winner == "Player"
+			? playerPickParagraph.classList.add("text-blue")
+			: playerPickParagraph.classList.add("text-pink");
+		winner == "Computer"
+			? computerPickParagraph.classList.add("text-blue")
+			: computerPickParagraph.classList.add("text-pink");
+	}
+
+	const playerElement = document.querySelector("#playerPick");
+	const computerElement = document.querySelector("#computerPick");
+	playerElement.appendChild(playerPickParagraph);
+	computerElement.appendChild(computerPickParagraph);
+
+	return winner;
 }
 
 function increaseScore(winner) {
-    switch (winner) {
-        case ("Player"):
-            playerScore += 1;
-            document.getElementById('playerScore').innerHTML = playerScore;
-            break;
-        case ("Computer"):
-            computerScore += 1;
-            document.getElementById('computerScore').innerHTML = computerScore;
-            break;
-    }
+	switch (winner) {
+		case "Player":
+			playerScore += 1;
+			document.getElementById("playerScore").innerHTML = playerScore;
+			break;
+		case "Computer":
+			computerScore += 1;
+			document.getElementById("computerScore").innerHTML = computerScore;
+			break;
+	}
 
-    combinedScore = playerScore + computerScore;
+	combinedScore = playerScore + computerScore;
 }
 
 function announceWinner() {
-    if (playerScore > computerScore) {
-        overallWinner = "Player";
-    } else {
-        overallWinner = "Computer";
-    }
+	if (playerScore > computerScore) {
+		overallWinner = "Player";
+	} else {
+		overallWinner = "Computer";
+	}
 
-    finalGameMessage = `Final score: ${playerScore} to ${computerScore}, ${overallWinner}`;
-    alert(finalGameMessage);
+	finalGameMessage = `Final score: ${playerScore} to ${computerScore}, ${overallWinner}`;
+	alert(finalGameMessage);
 }
 
 function resetGameLog(id) {
-    const parent = document.getElementById(id);
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+	const parent = document.getElementById(id);
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
+	}
 }
 
 function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    combinedScore = 0;
+	playerScore = 0;
+	computerScore = 0;
+	combinedScore = 0;
 
-    document.getElementById('playerScore').innerHTML = playerScore;
-    document.getElementById('computerScore').innerHTML = computerScore;
-    resetGameLog('playerPick');
-    resetGameLog('computerPick');
+	document.getElementById("playerScore").innerHTML = playerScore;
+	document.getElementById("computerScore").innerHTML = computerScore;
+	resetGameLog("playerPick");
+	resetGameLog("computerPick");
 }
 
 function play(userSelection) {
-    if (combinedScore < 5) {
-        winner = playRound(userSelection);
-        increaseScore(winner);
-    } else {
-        announceWinner();
-        resetGame();
-    }
+	if (combinedScore < 5) {
+		winner = playRound(userSelection);
+		increaseScore(winner);
+	} else {
+		announceWinner();
+		resetGame();
+	}
 }
 
-/// Event Listeners /// 
-const rock = document.getElementById('rock');
-rock.addEventListener('click', () => {
-    play("rock");
+/// Event Listeners ///
+const rock = document.getElementById("rock");
+rock.addEventListener("click", () => {
+	play("rock");
 });
 
-const paper = document.getElementById('paper');
-paper.addEventListener('click', () => {
-    play("paper");
+const paper = document.getElementById("paper");
+paper.addEventListener("click", () => {
+	play("paper");
 });
 
-const scissors = document.getElementById('scissors');
-scissors.addEventListener('click', () => {
-    play("scissors");
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", () => {
+	play("scissors");
 });
 
-const restart = document.getElementById('restartButton');
-restart.addEventListener('click', () => {
-    resetGame();
+const restart = document.getElementById("restartButton");
+restart.addEventListener("click", () => {
+	resetGame();
 });
